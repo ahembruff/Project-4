379e1105301ecb59255fe1a252f8bb99dae40820
ahembruff <168450462+ahembruff@users.noreply.github.com>
2024-12-12 23:27:01 -0500

    Troubleshooting to get outputs to be correct

--- HembruffAidan_Project4.py
+++ HembruffAidan_Project4.py
@@ -97,8 +97,9 @@ def sch_eqn(nspace,ntime,tau,method="ftcs",length=200,potential=[],wparam=[10,0,
     H[0,-1] = H_coeff ; H[0,0] = -2*H_coeff ; H[0,1] = H_coeff
     H[-1,2] = H_coeff ; H[-1,-1] = -2*H_coeff ; H[-1,0] = H_coeff
     
-    for i in potential:
-        H[i,i] += 1  
+    if len(potential) != 0:
+        for i in potential:
+            H[i,i] += 1  
     
     x = np.linspace(-length/2,length/2,nspace)
     t = tau*np.arange(0,ntime) # the time grid
@@ -108,7 +109,7 @@ def sch_eqn(nspace,ntime,tau,method="ftcs",length=200,potential=[],wparam=[10,0,
         probability = np.zeros((ntime))
         
         # below from Lab 11
-        psi = np.zeros((nspace,ntime)) # initialize the array for storing the complete spatial solution
+        psi = np.zeros((nspace,ntime),dtype=complex) # initialize the array for storing the complete spatial solution
         
         # initial condition using make_initialcond function developed in Lab 10
         psi[:,0] = make_initialcond(x,k0,sigma0)
@@ -137,26 +138,26 @@ def sch_eqn(nspace,ntime,tau,method="ftcs",length=200,potential=[],wparam=[10,0,
         
         probability = np.zeros((ntime))
         
-        psi = np.zeros((nspace,ntime))
+        psi = np.zeros((nspace,ntime),dtype=complex)
         
         # initial condition using make_initialcond function developed in Lab 10
         psi[:,0] = make_initialcond(x,k0,sigma0)
         
-        crank_A = inv(np.identity(nspace)+crank_coeff*H).dot(np.identity(nspace)-crank_coeff*H)
+        crank_A = np.dot(inv(np.identity(nspace)+crank_coeff*H),(np.identity(nspace)-crank_coeff*H))
         
         # following 7 lines adapted from Lab 11
         # iterate over the number of time steps to obtain spatial solutions for every time step
         for istep in range(1, ntime):
-            psi[:,istep] = crank_A.dot(psi[:,istep-1])
+            psi[:,istep] = np.dot(crank_A,psi[:,istep-1])
             
             #probability[istep] = psi[:,istep]*(np.conjugate(psi[:,istep]))
     
-    return psi, x, t, #probability
+    return psi, x, t #probability
 
-sol = sch_eqn(80,200,1,"crank")[0]
+sol = sch_eqn(80,200,1,"crank")
 x =  sol[1]
 psi = sol[0]
-index = 50
+index = 0
 
 
 def sch_plot(plot_type="psi",save=True,filepath="HembruffAidan_Project4_Fig1.png"):
